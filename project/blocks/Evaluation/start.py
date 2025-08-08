ce6d24ef8507d7a1c724f583ffa3f0224e46d1e7
ybkim453 <yb1553@dgu.ac.kr>
2025-08-08 03:17:20 +0000

    Evaluate

---
+++ project/blocks/Evaluation/start.py
@@ -0,0 +1,13 @@
+# blocks/Evaluation/start.py
+
+def generate_evaluation_snippet(form):
+
+    lines = []
+    # 학습 모듈 import
+    lines.append("# -----------------------------")
+    lines.append("# --------- [평가하기 블록] ---------")
+    lines.append("# -----------------------------")
+    lines.append("")
+
+
+    return "\n".join(lines)
